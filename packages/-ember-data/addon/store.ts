807a231a1ef380e74c6e251f51376f918c2b1f6b
Chris Thoburn <cthoburn@linkedin.com>
2019-09-26 17:50:26 -0700

    use typescript

--- packages/-ember-data/addon/store.ts
+++ packages/-ember-data/addon/store.ts
@@ -3,8 +3,10 @@ import { RecordData } from '@ember-data/record-data/-private';
 import { identifierCacheFor } from '@ember-data/store/-private';
 import { IDENTIFIERS } from '@ember-data/canary-features';
 
+type RecordDataStoreWrapper = import('@ember-data/store/-private/ts-interfaces/record-data-store-wrapper').RecordDataStoreWrapper;
+
 export default class DefaultStore extends Store {
-  createRecordDataFor(modelName, id, clientId, storeWrapper) {
+  createRecordDataFor(modelName: string, id: string | null, clientId: string, storeWrapper: RecordDataStoreWrapper) {
     if (IDENTIFIERS) {
       let identifier = identifierCacheFor(this).getOrCreateRecordIdentifier({
         type: modelName,
